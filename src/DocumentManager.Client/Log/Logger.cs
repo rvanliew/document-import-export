91a963eb478fb879298055bbd8608fe069755e19
Kenneth Watkins <vercore5866@gmail.com>
2022-04-08 02:52:34 -0400

    Added dropped Log folder

---
+++ src/DocumentManager.Client/Log/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace DocumentExportUtility_API.Log
+{
+    public class Logger
+    {
+        public string path;
+
+        public Logger()
+        {
+            //
+        }
+
+        public void CreateLog(StringBuilder sb)
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string logFolder = Path.Combine(folder, "LoganDocumentExport", "_Log");
+            Directory.CreateDirectory(logFolder);
+
+            path = $@"{logFolder}{GlobalValues.FileLocation.log}";
+
+            File.AppendAllText(path, sb.ToString());
+        }
+    }
+}
